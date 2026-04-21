e620c63cce1a8011adb96047ec73fc545f7909c8
xpgenie1 <xpgenie1@gmail.com>
2026-04-21 16:32:00 +0100

    Delete v4 directory

--- v4/private/class/USU/USUCliente.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'v4/private/class/CLI/CLICliente.php';
-/**
- * USUCliente alias de CLICliente
- * @package CLI
- * @deprecated
- */
-class USUCliente extends CLICliente{}
-?>
--- v4/private/class/UTL/TPL/UTLExportarOU.tpl
+++
@@ -1,5 +0,0 @@
-
-<form name="export-25" method="POST">
-	<input type="hidden" name="accion" value="exportar_25">
-	<input type="submit" name="enviar" value="Exportar Gratuitos 25"/>
-</form>
